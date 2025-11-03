822b8513598f39dc22ae98553fa9421c167fbbba
michal92299 <chudybyczek2137@gmail.com>
2025-11-03 13:56:27 +0100

    Update unpack.sh

--- updates/unpack.sh
+++ updates/unpack.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+sudo rm -rf /usr/share/LegendaryOS/
+sudo mv /tmp/LegendaryOS/updates/LegendaryOS/ /usr/share/
+cd /usr/share/LegendaryOS/SCRIPTS/
+sudo mkdir LOS-CLI
+sudo mkdir LCR
+sudo chmod a+x ./legendaryos-upgrade
+cd LOS-CLI
 
