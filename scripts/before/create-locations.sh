476d472e8d28e96cfc9bc690b8d658d97fad0272
michal92299 <chudybyczek2137@gmail.com>
2026-08-09 16:09:35 +0200

    Update create-locations.sh

--- scripts/before/create-locations.sh
+++ scripts/before/create-locations.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo mkdir /usr/share/LegendaryOS/tools/
-sudo mkdir /ust/share/LegendaryOS/icons/
+# sudo mkdir /usr/share/LegendaryOS/tools/
+# sudo mkdir /ust/share/LegendaryOS/icons/
